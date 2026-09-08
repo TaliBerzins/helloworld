4e5337ca4c967046077e62185e53c787af2267a5
TaliBerzins <tali.berzins@outlook.com>
2026-09-08 17:47:24 -0400

    Initialized file

---
+++ hellworld.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file Lab1_tberzinscpp
+ * @author Tali Berzins
+ * @date 2026-09-8
+ * @brief A simple program to greet the user by name.
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
